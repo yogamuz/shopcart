206c804c516b55f9d39aad165c2807034dc2d8e9
yogamuz <yogamuz13@gmail.com>
2025-10-14 18:29:02 +0700

    typo

--- src/router/index.js
+++ src/router/index.js
@@ -137,7 +137,7 @@ const routes = [
   {
     path: "/admin/dashboard",
     name: "AdminDashboard",
-    component: () => import("../views/adminDashboard.vue"),
+    component: () => import("../views/AdminDashboard.vue"),
     meta: {
       requiresAuth: true,
       role: "admin",
